f41e078e504a0993b2ae72913baa2160fd5420db
johnugbor <ugborjohn@gmail.com>
2021-11-03 14:36:28 +0100

    added email details

--- backend/settings.py
+++ backend/settings.py
@@ -125,7 +125,7 @@ PRODUCTION_DATABASES = {
 }
 
 
-DEFAULT_FROM_EMAIL ='chockoschockos1@gmail.com'
+DEFAULT_FROM_EMAIL ='mrjohnugbor@gmail.com'
 #developmentdb1
 DEVELOPMENT_DATABASES1 = {
     'default': {
@@ -308,9 +308,9 @@ ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 REST_USE_JWT = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST_USER = 'chockoschockos1@gmail.com'
-EMAIL_HOST_PASSWORD = 'Billionchockos'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS= True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
