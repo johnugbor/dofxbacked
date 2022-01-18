0f11e2a088e9f7b372d91e9477b3e8a22502c20c
John Ugbor <ugborjohn@gmail.com>
2022-01-18 02:29:58 +0100

    Added prod config to settings

--- backend/settings.py
+++ backend/settings.py
@@ -331,7 +331,7 @@ REST_USE_JWT = True
 
 #heroku gmail config
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#
+
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS= True
